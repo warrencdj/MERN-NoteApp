d6a43cdf20dfc05143e6983c4ba49c6bc39440a4
warrencdj <warrencdj145@gmail.com>
2026-01-18 22:22:40 +0800

    Prepare for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,8 @@ import dns from "node:dns/promises"
 
 export const connectDB = async () => {
   try {
-    dns.setServers(["1.1.1.1"])
+    // Allow fallback to system DNS if 1.1.1.1 is unreachable
+    dns.setServers(["1.1.1.1", "8.8.8.8"])
 
     await mongoose.connect(process.env.MONGO_URI)
 
@@ -12,4 +13,4 @@ export const connectDB = async () => {
     console.error("Error connecting to DB", error)
     process.exit(1)
   }
-};
+}
